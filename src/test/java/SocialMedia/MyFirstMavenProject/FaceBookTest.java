d327b59ab555f8e6683a5608d6a996190df2feae
zahidny1 <mdzahidhossain813@gmail.com>
2020-06-28 03:48:49 -0400

    My First Commit

---
+++ src/test/java/SocialMedia/MyFirstMavenProject/FaceBookTest.java
@@ -0,0 +1,25 @@
+package SocialMedia.MyFirstMavenProject;
+
+import org.testng.annotations.Test;
+
+public class FaceBookTest {
+	@Test(priority=1)
+	public void OpenBroTest() {
+		System.out.println("Browser Opens.");
+	}
+	@Test(priority=2)
+	public void LogInTest() {
+		System.out.println("Successfully Log in.");
+
+}
+	@Test(priority=3)
+	public void LogOutTest() {
+		System.out.println("Successfully Log out.");
+		
+		
+	}
+	@Test(priority=4)
+	public void CloseBroTest() {
+		System.out.println("browser close Successfully.");
+	}		
+}
---
+++ src/test/java/SocialMedia/MyFirstMavenProject/InstagramTest.java
@@ -0,0 +1,26 @@
+package SocialMedia.MyFirstMavenProject;
+
+import org.testng.annotations.Test;
+
+public class InstagramTest {
+	@Test(priority=1)
+	public void OpenBroTest() {
+		System.out.println("Browser Opens.");
+	}
+	@Test(priority=2)
+	public void LogInTest() {
+		System.out.println("Successfully Log in.");
+
+}
+	@Test(priority=3)
+	public void LogOutTest() {
+		System.out.println("Successfully Log out.");
+		
+		
+	}
+	@Test(priority=4)
+	public void CloseBroTest() {
+		System.out.println("browser close Successfully.");
+
+}
+}
